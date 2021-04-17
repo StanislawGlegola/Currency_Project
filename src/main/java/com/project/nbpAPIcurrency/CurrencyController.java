91d1004f8ed5bc7f1348eb20bfcbbaea39e97fcf
DLXP3N0 <sta.glegola@gmail.com>
2021-04-17 21:15:19 +0200

    Variables changed to more adequate

--- src/main/java/com/project/nbpAPIcurrency/CurrencyController.java
+++ src/main/java/com/project/nbpAPIcurrency/CurrencyController.java
@@ -1,37 +1,27 @@
 package com.project.nbpAPIcurrency;
 
-import com.project.nbpAPIcurrency.model.Currency;
+import com.project.nbpAPIcurrency.dto.ExchangeRatesTableDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import java.io.IOException;
 
 @Controller
 public class CurrencyController {
 
-private final CurrencyService currencyService;
+    private final CurrencyService currencyService;
 
     public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
     }
 
-    /*@GetMapping("/login")
-    public String currencyGo(Model model) {
-
-        List<Currency> currencyList = currencyService.getCurrencyList().getCurrencyList();
-        String getNumber = currencyService.getCurrencyList().getNumber();
-        model.addAttribute("currencyList", currencyList);
-        model.addAttribute("getValueJakis", getNumber);
-        return "currency";
-    }*/
-
     @GetMapping("/")
-    public String currency(Model model) {
-        List<Currency> currencyList = currencyService.getXMLList();
-        String getNumber = currencyService.getCurrencyList().getNumber();
-        model.addAttribute("currencyList", currencyList);
-        model.addAttribute("getValueJakis", getNumber);
+    public String currency(Model model) throws IOException {
+        ExchangeRatesTableDTO currencyList = currencyService.getCurrencyList();
+        //String getNumber = currencyService.getCurrencyList().getNumber();
+        model.addAttribute("ratesList", currencyList.getRates());
+        //model.addAttribute("getValueJakis", getNumber);
         return "view/currency";
     }
 }
