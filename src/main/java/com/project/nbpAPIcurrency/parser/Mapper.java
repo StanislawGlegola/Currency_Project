a4156f755aaba0a9153a6e35b4955eafce35cd19
DLXP3N0 <sta.glegola@gmail.com>
2021-04-17 21:23:24 +0200

    As spring component

--- src/main/java/com/project/nbpAPIcurrency/parser/Mapper.java
+++ src/main/java/com/project/nbpAPIcurrency/parser/Mapper.java
@@ -2,16 +2,17 @@ package com.project.nbpAPIcurrency.parser;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.nbpAPIcurrency.dto.ExchangeRatesTableDTO;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class Mapper {
 
-    public static ExchangeRatesTableDTO mapperThenDto() throws IOException {
+    public ExchangeRatesTableDTO mapperThenDto() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         String json = JsonReader.getDataFromUrl("http://api.nbp.pl/api/exchangerates/tables/a?format=json");
         ExchangeRatesTableDTO exchangeRatesTableDTO = objectMapper.readValue(json, ExchangeRatesTableDTO.class);
-        //String table = exchangeRatesTableDTO.getTable();
         return exchangeRatesTableDTO;
     }
 
