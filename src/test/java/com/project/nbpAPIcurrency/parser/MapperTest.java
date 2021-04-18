f08f34c94d75e255c242dd8f5e35324688ca8785
DLXP3N0 <sta.glegola@gmail.com>
2021-04-18 20:13:11 +0200

    updated tests

--- src/test/java/com/project/nbpAPIcurrency/parser/MapperTest.java
+++ src/test/java/com/project/nbpAPIcurrency/parser/MapperTest.java
@@ -2,22 +2,19 @@ package com.project.nbpAPIcurrency.parser;
 
 import org.junit.jupiter.api.Test;
 import com.project.nbpAPIcurrency.dto.ExchangeRatesTableDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
+import static com.project.nbpAPIcurrency.parser.Mapper.mapperThenDto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MapperTest {
 
-    @Autowired
-    Mapper mapper;
-
     @Test
     void shouldReturnTableALetter() throws IOException {
-        ExchangeRatesTableDTO exchangeRatesTableDTO = mapper.mapperThenDto();
+        ExchangeRatesTableDTO exchangeRatesTableDTO = mapperThenDto();
         String table = exchangeRatesTableDTO.getTable();
         assertEquals(table, "A");
         //System.out.println(table);
